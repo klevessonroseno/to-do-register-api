823968681ca5e233064e3f79303ebc57eb59c86c
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-21 13:49:22 -0300

    Fazendo validação no momento de salvar um usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,19 @@
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class UserController {
     async store(req, res){
         try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                email: Yup.string().email().required(),
+                password: Yup.string().required().min(6)
+            });
+
+            if(!(await schema.isValid(req.body))){
+                return res.status(400).json({ error: 'Validation Fails' });    
+            }
+
             const userExists = await User.findOne({
                 where: { email: req.body.email }
             });
