f4fa4eac6021780aa37f3675c8c44b5e6e9dab2b
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-21 11:46:53 -0300

    Arquivos para configuração da conexão com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    database: 'to_do_register',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+};
