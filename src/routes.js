c509c11bdc05f9f2b9940b3f73ca6dc212bfd20f
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-22 20:27:12 -0300

    Importando Controller de Files no arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -15,6 +17,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', authMiddleware, UserController.update);
-routes.post('/files', upload.single('file'));
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
