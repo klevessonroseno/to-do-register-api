edc8efff627c8398e6803962bd7ef32f67909e4b
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-22 21:14:58 -0300

    Criação de Model de File

---
+++ src/app/models/File.js
@@ -0,0 +1,16 @@
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+    static init(connection) {
+        super.init({
+            name: Sequelize.STRING,
+            path: Sequelize.STRING,      
+        }, {
+            sequelize: connection,
+        });
+        
+        return this;
+    }
+}
+
+export default File;
